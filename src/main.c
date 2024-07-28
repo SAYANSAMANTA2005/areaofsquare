e6de2c185e3becf0a546ceef72e981d6eb91e20b
root <root@localhost>
2024-07-28 22:06:16 +0530

    auto publish 1722184575887

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+   
+    int a;
+    printf(" enter length of square ");
+    scanf("%d",&a);
+    
+ 
+ 
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
